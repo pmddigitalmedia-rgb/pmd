51d74e7872eadb650c2c26e3553cb8d4022a8cf4
pmddigitalmedia-rgb <pmddigitalmedia@gmail.com>
2025-12-02 13:22:08 -0800

    pmd editor

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Header } from './components/Header';
 import { Workspace } from './components/Workspace';
 import { SlotNavigation } from './components/SlotNavigation';
